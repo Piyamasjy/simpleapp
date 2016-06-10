288d7b6bdbcb6d98bbddc522b425aecdac080154
piyamas <piyamas.mine@gmail.com>
2016-06-10 15:00:13 +0700

    Add folder simpleapp

---
+++ config/routes.rb
@@ -0,0 +1,43 @@
+Rails.application.routes.draw do
+  
+
+get'/users/show/:id', to:'users#show', as: 'users_show'
+get'/users/new', to:'users#new', as: 'users_new'
+post'/users/create', to:'users#create', as: 'users_create'
+get'/users/log_in', to:'users#log_in', as: 'users_log_in'
+post'/users/create_session', to:'users#create_session', as: 'users_create_session'
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
